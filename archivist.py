501f4863e4f90a0751d0f218e2d5648473455c4e
drake <gusaxedr@student.gu.se>
2021-03-25 21:51:00 +0100

    basics

---
+++ archivist.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+import os
+from isbntools.app import *
+import urllib.request
+import json
+import datetime
+import textwrap
+
+with open("./config.json") as f:
+    data = json.load(f)
+    token = data["TOKEN"]
+    prefix = data["PREFIX"]
+
+client = commands.Bot(command_prefix = prefix)
+base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+
+@client.event
+async def on_ready() :
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Reading"))
+    print("I am online")
+
+@client.command()
+async def find(ctx, *, title):
+  get_isbn = isbn_from_words(title)
+  
+  try:
+    with urllib.request.urlopen(base_api_link + get_isbn) as f:
+          text = f.read()
+    decoded_text = text.decode("utf-8")
+    obj = json.loads(decoded_text) # deserializes decoded_text to a Python object
+    volume_info = obj["items"][0] 
+    authors = obj["items"][0]["volumeInfo"]["authors"]
+
+    embed = discord.Embed(colour=discord.Colour(0xf5a623), timestamp=datetime.datetime.utcnow())
+    embed.add_field(name="Title", value=volume_info["volumeInfo"]["title"], inline=False)
+    embed.add_field(name="ISBN", value=get_isbn, inline=true)
+    embed.add_field(name="Authors", value=",\n".join(authors), inline=True)
+    embed.add_field(name="Public Domain ", value=volume_info["accessInfo"]["publicDomain"], inline=True)
+    embed.add_field(name="Page count", value=volume_info["volumeInfo"]["pageCount"])
+    embed.add_field(name="Summary", value=volume_info["searchInfo"]["textSnippet"])
+  except:
+    await ctx.send("look I went to the Library of Alexandria and I could not find your book, I hate to ask but are you sure thats how it is spelled?")
+
+  await ctx.send(embed=embed)
+
+
+
+
+client.run(token)
