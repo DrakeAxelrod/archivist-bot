7ff8406852aff7e018ae2a2da681128c8bccc365
Drake <drakeaxelrod@gmail.com>
2021-03-26 17:07:06 +0100

    some formatting

--- archivist.py
+++ archivist.py
@@ -4,6 +4,7 @@ import os
 from isbntools.app import *
 import urllib.request
 import json
+import re
 import datetime
 import dotenv 
 from dotenv import load_dotenv
@@ -16,6 +17,13 @@ client = commands.Bot(command_prefix = ".")
 base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
 
 client.remove_command("help")
+
+def strip_symbols(strip_string):
+  a = re.sub('&quot;', '"', strip_string)
+  b = re.sub('&#39;', "'", a)
+  c = re.sub('--Provided in synopsis.', "", b)
+  d = re.sub('Provided in synopsis.', "", c)
+  return d
 
 @client.event
 async def on_ready() :
@@ -36,13 +44,15 @@ async def find(ctx, *, title):
     volume_info = obj["items"][0] 
     authors = obj["items"][0]["volumeInfo"]["authors"]
 
+    clean_summery = strip_symbols(volume_info["searchInfo"]["textSnippet"])
+
     embed = discord.Embed(colour=discord.Colour(0xf5a623), timestamp=datetime.datetime.utcnow())
     embed.add_field(name="Title", value=volume_info["volumeInfo"]["title"], inline=True)
     embed.add_field(name="ISBN", value=get_isbn, inline=False)
     embed.add_field(name="Authors", value=",\n".join(authors), inline=True)
     embed.add_field(name="Public Domain ", value=volume_info["accessInfo"]["publicDomain"], inline=True)
     embed.add_field(name="Page count", value=volume_info["volumeInfo"]["pageCount"], inline=True)
-    embed.add_field(name="Summary", value=volume_info["searchInfo"]["textSnippet"])
+    embed.add_field(name="Summary", value=clean_summery)
   except:
     await ctx.send("look I went to the Library of Alexandria and I could not find your book, I hate to ask but are you sure thats how it is spelled?")
   await ctx.send(embed=embed)
