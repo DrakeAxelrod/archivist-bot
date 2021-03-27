36ed8c205dcc5b2cc1a4a1ffeb1f968a7a6a59a4
drake <gusaxedr@student.gu.se>
2021-03-27 11:23:28 +0100

    fixed help again

--- cogs/util.py
+++ cogs/util.py
@@ -6,11 +6,11 @@ class Util(commands.Cog):
   def __init__(self, client):
     self.client = client
 
-  @commands.command()
-  async def help(self, ctx):
-    embed = discord.Embed(colour=discord.Colour(0xf5a623), timestamp=datetime.datetime.utcnow())
-    embed.add_field(name=".find <search term>", value="replace <search term> with author, title, isbn, or anything really. Archivist will attempt to find your book!" )
-    await ctx.send(embed=embed)
+  # @commands.command()
+  # async def help(self, ctx):
+  #   embed = discord.Embed(colour=discord.Colour(0xf5a623), timestamp=datetime.datetime.utcnow())
+  #   embed.add_field(name=".find <search term>", value="replace <search term> with author, title, isbn, or anything really. Archivist will attempt to find your book!" )
+  #   await ctx.send(embed=embed)
 
 def setup(client):
   client.add_cog(Util(client))
