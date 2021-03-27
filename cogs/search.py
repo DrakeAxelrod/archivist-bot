80885e9d5dd2e573bd2203a0b43afa16ac4c9c1f
drake <gusaxedr@student.gu.se>
2021-03-27 15:43:05 +0100

    support for translations

--- cogs/search.py
+++ cogs/search.py
@@ -7,26 +7,56 @@ import random
 import datetime
 import discord
 from discord.ext import commands
+from google_trans_new import google_translator
 
 class Search(commands.Cog):
+  base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+  translator = google_translator()
 
   def __init__(self, client):
     self.client = client
 
 
-  def strip_symbols(strip_string):
+  def strip_symbols(self, ctx, strip_string):
     a = re.sub('&quot;', '"', strip_string)
     b = re.sub('&#39;', "'", a)
     c = re.sub('--Provided in synopsis.|Provided in synopsis.', "", b)
     return c
+  
+  def lang_code(self, ctx, string):
+    formated_string = string.capitalize()
+    with open('./languages.json') as json_lang:
+      langs = json.load(json_lang)
+      return langs[formated_string]
+
+  def seperateArgs(self, ctx, args, delimeter):
+    finalArgs = []
+    toAppend = ''
+    index = 0
+    for i in args:
+        if(i == delimeter):
+            finalArgs.append(toAppend.strip())
+            toAppend = ''
+        else:
+            toAppend += i
+        if(index == len(args) - 1):
+            finalArgs.append(toAppend.strip())
+            toAppend = ''
+        index += 1
+    return finalArgs
 
   @commands.command()
-  async def find(self, ctx, *, question):
-    get_isbn = isbn_from_words(question)
+  async def find(self, ctx, *,question):
+    string = str(question)
+    print(string)
+    args = self.seperateArgs(ctx, string, "-")
 
-    base_api_link = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
-  
-    with urllib.request.urlopen(base_api_link + get_isbn) as f:
+    get_isbn = isbn_from_words(args[0])
+    if len(args) > 1:
+      lang = self.lang_code(ctx, args[1])
+    else:
+      lang = 'en'
+    with urllib.request.urlopen(self.base_api_link + get_isbn) as f:
       query = f.read()
 
     decoded_query = query.decode("utf-8")
@@ -37,27 +67,56 @@ class Search(commands.Cog):
         authors_data = results["volumeInfo"]["authors"]
       else:
         author_data = "unknown"
+      
+      # image_url = results["volumeInfo"]["imageLinks"]["thumbnail"]
+      # book_title = results["volumeInfo"]["title"]
+      # authors = ",\n".join(authors_data)
+      # public_domain = results["accessInfo"]["publicDomain"]
+      # page_count = results["volumeInfo"]["pageCount"]
+      # description = results["volumeInfo"]["description"]
+      # print("web url: " + str(weblink))
+      # print("img url: " + str(image_url))
+      # print("title: " + str(book_title))
+      # print("author: " + str(authors))
+      # print("domain: " + str(public_domain))
+      # print("pg: " + str(page_count))
+      # print("description: " + str(description))
 
       embed_color = discord.Colour(0xf5a623)
       time = datetime.datetime.utcnow()
+
       if "previewLink" in results["volumeInfo"]:
         weblink = results["volumeInfo"]["previewLink"]
         embed = discord.Embed(colour=embed_color, description=f"[More Information]({weblink})", timestamp=time)
       else:
         embed = discord.Embed(colour=embed_color, timestamp=time)
+
       if "imageLinks" in results["volumeInfo"]:
         embed.set_thumbnail(url=results["volumeInfo"]["imageLinks"]["thumbnail"])
+
       if "title" in results["volumeInfo"]:
-        embed.add_field(name="Title", value=results["volumeInfo"]["title"], inline=False)
+        pre_title = results["volumeInfo"]["title"]
+        original_lang = self.translator.detect(pre_title)
+        title = self.translator.translate(pre_title, lang_tgt=lang)
+        embed.add_field(name="Original Language", value=f"{original_lang[1]} - {original_lang[0]}", inline=False)
+        embed.add_field(name="Title", value=title, inline=False)
+
       embed.add_field(name="ISBN", value=get_isbn, inline=False)
       embed.add_field(name="Authors", value=",\n".join(authors_data), inline=True)
+
       if "publicDomain" in results["accessInfo"]:
         embed.add_field(name="Public Domain ", value=results["accessInfo"]["publicDomain"], inline=True)
+
       if "pageCount" in results["volumeInfo"]:
         embed.add_field(name="Page count", value=results["volumeInfo"]["pageCount"], inline=True)
+
       if "description" in results["volumeInfo"]:
-        embed.add_field(name="Summary", value=results["volumeInfo"]["description"])
+        pre_description = results["volumeInfo"]["description"]
+        description = self.translator.translate(pre_description, lang_tgt=lang)
+        embed.add_field(name="Summary", value=description)
+
       await ctx.send(embed=embed)
+
     else:
       ran_search = ("checked", "looked in", "searched", "explored", "hunted throughout")
       ran_lib = ("The Library of Alexandria", "The Library of Ashurbanipal", "The Library of Pergamum", "The Villa of the Papyri", "The Libraries of Trajan’s Forum", "The Library of Celsus", "The Imperial Library of Constantinople", "The House of Wisdom")
--- cogs/util.py
+++ cogs/util.py
@@ -6,11 +6,12 @@ class Util(commands.Cog):
   def __init__(self, client):
     self.client = client
 
-  # @commands.command()
-  # async def help(self, ctx):
-  #   embed = discord.Embed(colour=discord.Colour(0xf5a623), timestamp=datetime.datetime.utcnow())
-  #   embed.add_field(name=".find <search term>", value="replace <search term> with author, title, isbn, or anything really. Archivist will attempt to find your book!" )
-  #   await ctx.send(embed=embed)
+  @commands.command()
+  async def help(self, ctx):
+    embed = discord.Embed(colour=discord.Colour(0xf5a623), timestamp=datetime.datetime.utcnow())
+    embed.add_field(name=".find <search term>", value="replace <search term> with author, title, isbn, or anything really. Archivist will attempt to find your book!\n", inline=False)
+    embed.add_field(name=".find <search term -language>", value='.find <your search term -english>. The default language is english if no flag is passed', inline=False)
+    await ctx.send(embed=embed)
 
 def setup(client):
   client.add_cog(Util(client))
